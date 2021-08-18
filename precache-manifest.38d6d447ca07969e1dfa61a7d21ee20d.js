ba1af573aab401c13504dee739ed803bcdc6c1c9
GiantBabyCorn <giantbabycorn@gmail.com>
2021-08-18 15:05:09 +0800

    init

---
+++ precache-manifest.38d6d447ca07969e1dfa61a7d21ee20d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3e7982bf41e915e156bdbb4e1d0aec",
+    "url": "/popcat-demo/index.html"
+  },
+  {
+    "revision": "64e2543dcc4ef30aab8a",
+    "url": "/popcat-demo/static/css/5.d2188292.chunk.css"
+  },
+  {
+    "revision": "60432ca40141c3412088",
+    "url": "/popcat-demo/static/css/main.806198b7.chunk.css"
+  },
+  {
+    "revision": "c710f59b6ef754ea33dc",
+    "url": "/popcat-demo/static/js/2.92a83ab2.chunk.js"
+  },
+  {
+    "revision": "9141e6603b2ba65e9f944cb50ce12050",
+    "url": "/popcat-demo/static/js/2.92a83ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebe107f1f562234605d",
+    "url": "/popcat-demo/static/js/3.a4cf4cd8.chunk.js"
+  },
+  {
+    "revision": "e1a15f3ac9ac1f3abbbb282780755415",
+    "url": "/popcat-demo/static/js/3.a4cf4cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3186dc94ba3f013a5494",
+    "url": "/popcat-demo/static/js/4.a458758a.chunk.js"
+  },
+  {
+    "revision": "64e2543dcc4ef30aab8a",
+    "url": "/popcat-demo/static/js/5.799a4065.chunk.js"
+  },
+  {
+    "revision": "7dd6ce91bfc3eb189852",
+    "url": "/popcat-demo/static/js/6.110ce6e1.chunk.js"
+  },
+  {
+    "revision": "60432ca40141c3412088",
+    "url": "/popcat-demo/static/js/main.d76c92ad.chunk.js"
+  },
+  {
+    "revision": "0dcae97d6317978672c6",
+    "url": "/popcat-demo/static/js/runtime-main.a95e82d0.js"
+  },
+  {
+    "revision": "7ce6f55c00e6523b17ae22dff37f86f2",
+    "url": "/popcat-demo/static/media/delete-button.7ce6f55c.svg"
+  },
+  {
+    "revision": "8d5694f660709021fcbbd7cd2507a9e8",
+    "url": "/popcat-demo/static/media/dice-button.8d5694f6.svg"
+  },
+  {
+    "revision": "8f03b22c356a1ce5bd6559bb2e7381e6",
+    "url": "/popcat-demo/static/media/donate-icon.8f03b22c.svg"
+  },
+  {
+    "revision": "812c50feba26633cc29d5187b2305dc5",
+    "url": "/popcat-demo/static/media/icon-fail.812c50fe.svg"
+  },
+  {
+    "revision": "c04c757fa6658d5bacc0870ce2a794fe",
+    "url": "/popcat-demo/static/media/icon-success.c04c757f.svg"
+  },
+  {
+    "revision": "a65d9a9e74b10df65328cd70e19a8289",
+    "url": "/popcat-demo/static/media/metamask.a65d9a9e.svg"
+  }
+]);
---
+++ static/js/6.110ce6e1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgiantbabycorn.finance"]=this["webpackJsonpgiantbabycorn.finance"]||[]).push([[6],{669:function(n,a){}}]);
+//# sourceMappingURL=6.110ce6e1.chunk.js.map
